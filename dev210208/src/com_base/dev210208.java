d3df99bc1594dda38df52e0cc8467a8cf831d6f2
kutaehwan <lion0523@naver.com>
2021-02-08 15:52:22 +0900

    1차 수정입니다.

---
+++ dev210208/src/com_base/dev210208.java
@@ -0,0 +1,14 @@
+package com_base;
+
+public class dev210208 {
+	int age = 20;
+
+	public static void main(String[] args) {
+		dev210208 hw = new dev210208();
+		hw.age = 31;
+		System.out.println("당신의 나이는 " + hw.age + " 입니다.");
+		System.out.println("2차 수정한 부분입니다.");
+
+	}
+
+}
